72547b319629beaff33f92b3c2ae463f0331bab9
ohendarko <ohendarko@gmail.com>
2024-05-22 21:43:09 +0000

    labels and imouts

--- 0x01-react_intro/task_1/dashboard/src/App.js
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -10,11 +10,18 @@ function App() {
         <img src={holbertonLogo} className="App-logo" alt="Holberton logo" />
         <h1>School dashboard</h1>
       </header>
-      <body className="App-body">
+      <div className="App-body"> {/* Changed <body> to <div> */}
         <p>Login to access the full dashboard</p>
-      </body>
+        <label htmlFor="e-mail">Email</label> {/* Changed 'for' to 'htmlFor' */}
+        <input type="email" name="email" id="e-mail" />
+        <br />
+        <label htmlFor="pass-word">Password</label> {/* Changed 'for' to 'htmlFor' */}
+        <input type="password" id="pass-word" />
+        <br />
+        <button>OK</button>
+      </div> {/* Closed the <div> */}
       <footer className="App-footer">
-        <p>Copyright {getFullYear} - {getFooterCopy(true)}</p>
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p> {/* Added parentheses to function calls */}
       </footer>
     </div>
   );
