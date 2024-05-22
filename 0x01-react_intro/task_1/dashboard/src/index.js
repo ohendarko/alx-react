a1d00a9593a7b2e5803a09ac5aac03e369779ca4
ohendarko <ohendarko@gmail.com>
2024-05-22 21:31:27 +0000

    Notifications

--- 0x01-react_intro/task_1/dashboard/src/index.js
+++ 0x01-react_intro/task_1/dashboard/src/index.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Notifications from './Notifications';
+import Notifications from './Notifications.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Notifications />
   </React.StrictMode>
 );
 
 // Render the Notifications component
-const notificationsRoot = ReactDOM.createRoot(document.getElementById('root-notifications'));
-notificationsRoot.render(
-  <React.StrictMode>
-    <Notifications />
-  </React.StrictMode>
-)
+// const notificationsRoot = ReactDOM.createRoot(document.getElementById('root-notifications'));
+// notificationsRoot.render(
+//   <React.StrictMode>
+//     <Notifications />
+//   </React.StrictMode>
+// )
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
