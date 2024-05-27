d8857f27815c348ae95241c0423b7d73e39c9c62
ohendarko <ohendarko@gmail.com>
2024-05-27 23:03:29 +0000

    splitting notification components

--- 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.js
+++ 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Notifications.css";
 import closeIcon from './close-icon.png';
-import { getLatestNotification } from "../utils/utils";
+import NotificationItem from "./NotificationItem";
 
 export default function Notifications() {
   const handleCloseClick = () => {
@@ -12,9 +12,7 @@ export default function Notifications() {
     <div className="Notifications">
       <p>Here is the list of notifications</p>
       <ul>
-        <li data-priority="default">New course available</li>
-        <li data-priority="urgent">New resume available</li>
-        <li dangerouslySetInnerHTML={{ __html: getLatestNotification() }} />
+        <NotificationItem />
       </ul>
       <button
         style={{
