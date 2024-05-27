b00b41566f58f9791f1ee37f3ebc5d25790899d1
ohendarko <ohendarko@gmail.com>
2024-05-27 22:42:53 +0000

    added tests

--- 0x02-react_props/task_1/dashboard/src/App/App.test.js
+++ 0x02-react_props/task_1/dashboard/src/App/App.test.js
@@ -1,27 +1,29 @@
+// App.test.js
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
+import Notifications from './Notifications';
+import Header from './Header';
+import Login from './Login';
+import Footer from './Footer';
 
-describe('<App />', () => {
-  it('renders without crashing', () => {
-    render(<App />);
-  });
+test('renders Notifications component', () => {
+  const { getByText } = render(<App />);
+  expect(getByText(/Notifications/i)).toBeInTheDocument();
+});
 
-  it('renders a div with the class App-header', () => {
-    render(<App />);
-    const headerDiv = screen.getByRole('banner'); // Assuming that App-header div has a role of banner
-    expect(headerDiv).toHaveClass('App-header');
-  });
+test('renders Header component', () => {
+  const { getByText } = render(<App />);
+  expect(getByText(/Header/i)).toBeInTheDocument();
+});
 
-  it('renders a div with the class App-body', () => {
-    render(<App />);
-    const bodyDiv = screen.getByRole('main'); // Assuming that App-body div has a role of main
-    expect(bodyDiv).toHaveClass('App-body');
-  });
+test('renders Login component', () => {
+  const { getByText } = render(<App />);
+  expect(getByText(/Login/i)).toBeInTheDocument();
+});
 
-  it('renders a div with the class App-footer', () => {
-    render(<App />);
-    const footerDiv = screen.getByRole('contentinfo'); // Assuming that App-footer div has a role of contentinfo
-    expect(footerDiv).toHaveClass('App-footer');
-  });
+test('renders Footer component', () => {
+  const { getByText } = render(<App />);
+  expect(getByText(/Footer/i)).toBeInTheDocument();
 });
