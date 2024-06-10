96afd194e800ce5d5d4819f8c04ff97895d2fffa
ohendarko <ohendarko@gmail.com>
2024-06-10 23:21:41 +0000

    hook

--- 0x05-react_state/task_4/dashboard/src/CourseList/CourseListRow.js
+++ 0x05-react_state/task_4/dashboard/src/CourseList/CourseListRow.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { StyleSheet, css } from 'aphrodite';
 
 const CourseListRow = ({ isHeader, textFirstCell, textSecondCell = null }) => {
+  const [isChecked, setIsChecked] = useState(false);
+
   const headerStyle = { backgroundColor: '#deb5b545' };
-  const rowStyle = { backgroundColor: '#f5f5f5ab' };
+  const rowStyle = { backgroundColor: isChecked ? '#e6e4e4' : '#f5f5f5ab' };
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
 
   return (
     <tr style={isHeader ? headerStyle : rowStyle}>
@@ -19,7 +25,10 @@ const CourseListRow = ({ isHeader, textFirstCell, textSecondCell = null }) => {
         )
       ) : (
         <>
-          <td className={css(styles.td)}>{textFirstCell}</td>
+          <td className={css(styles.td)}>
+            <input type="checkbox" onChange={handleCheckboxChange} />
+            {textFirstCell}
+          </td>
           <td className={css(styles.td)}>{textSecondCell}</td>
         </>
       )}
@@ -51,6 +60,9 @@ const styles = StyleSheet.create({
   td: {
     border: '1px solid #ddd',
     padding: '8px',
+  },
+  rowChecked: {
+    backgroundColor: '#e6e4e4',
   },
 });
 
