2eed1581baad9f382895188fc015ac550df42ec6
ohendarko <ohendarko@gmail.com>
2024-06-12 21:45:01 +0000

    nested merge

---
+++ 0x06-React_Immutable/6-deeply.js
@@ -0,0 +1,10 @@
+import { List, Map } from 'immutable';
+
+export default function mergeDeeplyElements(page1, page2) {
+  const map1 = Map(page1);
+  const map2 = Map(page2);
+
+  const mergedMap = map1.mergeDeep(map2);
+
+  return List(mergedMap.valueSeq());
+}
