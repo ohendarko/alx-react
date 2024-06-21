432ade43d91895a03f36dc7c18811dfb81063342
ohendarko <ohendarko@gmail.com>
2024-06-21 14:25:57 +0000

    ui

---
+++ 0x07-react_redux_action_creator_normalizr/task_4/dashboard/src/CourseList/CourseList.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import CourseListRow from './CourseListRow';
+import CourseShape from './CourseShape';
+import { StyleSheet, css } from 'aphrodite';
+
+const CourseList = ({ listCourses }) => {
+  return (
+    <table className={css(styles.CourseList)}>
+      <thead>
+        <CourseListRow textFirstCell="Available courses" isHeader={true} />
+        <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
+      </thead>
+      <tbody>
+        {listCourses.length === 0 ? (
+          <CourseListRow textFirstCell="No course available yet" isHeader={false} />
+        ) : (
+          listCourses.map(course => (
+            <CourseListRow
+              key={course.id}
+              textFirstCell={course.name}
+              textSecondCell={course.credit}
+              isHeader={false}
+            />
+          ))
+        )}
+      </tbody>
+    </table>
+  );
+};
+
+CourseList.propTypes = {
+  listCourses: PropTypes.arrayOf(CourseShape),
+};
+
+CourseList.defaultProps = {
+  listCourses: [],
+};
+
+const styles = StyleSheet.create({
+  CourseList: {
+    width: '100%',
+    borderCollapse: 'collapse',
+  },
+  th: {
+    border: '1px solid #ddd',
+    padding: '8px',
+    textAlign: 'left',
+  },
+  td: {
+    border: '1px solid #ddd',
+    padding: '8px',
+  },
+});
+
+export default CourseList;
